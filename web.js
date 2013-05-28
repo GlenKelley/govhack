68d7393f1569d8cf72ce17581b5029aad27df96a
Glen Kelley <glen.t.kelley@gmail.com>
2013-05-28 12:11:56 +1000

    look ma

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hey Ma, look no hands! Email glen your public key');
 });
 
 var port = process.env.PORT || 5000;
