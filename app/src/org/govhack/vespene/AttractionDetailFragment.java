8308c8060dd815875cf4626fc6690f1111741557
Alex North <alex@alexn.id.au>
2013-06-01 15:52:42 +1000

    Detail layout

--- app/src/org/govhack/vespene/AttractionDetailFragment.java
+++ app/src/org/govhack/vespene/AttractionDetailFragment.java
@@ -1,5 +1,7 @@
 package org.govhack.vespene;
 
+import org.govhack.vespene.atlas.Product;
+
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,9 +9,26 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class AttractionDetailFragment extends Fragment {
+  private Product product = null;
+    
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+  
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, 
       Bundle savedInstanceState) {
+    getActivity().getActionBar().setTitle("");
     return inflater.inflate(R.layout.attraction_detail_layout, container, false);
+  }
+  
+  @Override
+  public void onStart() {
+    super.onStart();
+  }
+  
+  public void setProduct(Product product) {
+    this.product = product;
   }
 }
