dd9b4ee32b415c2e5d115c71e9b60fd0cfa76116
Glen Kelley <glen.t.kelley@gmail.com>
2013-06-01 15:07:56 +1000

    return null for missing json fields

--- app/src/org/govhack/vespene/atlas/Json.java
+++ app/src/org/govhack/vespene/atlas/Json.java
@@ -10,19 +10,27 @@ import org.json.JSONObject;
 public class Json {
 
   static JSONObject getJson(JSONObject json, String field) {
-	try {
-		return json.getJSONObject(field);
-	} catch (JSONException e) {
-	    throw new RuntimeException(e);
-	}		
+	if (json.isNull(field)) {
+		return null;
+	} else {
+		try {
+			return json.getJSONObject(field);
+		} catch (JSONException e) {
+		    throw new RuntimeException(e);
+		}		
+	}
   }
   
   static JSONArray getArray(JSONObject json, String field) {
-	try {
-		return json.getJSONArray(field);
-	} catch (JSONException e) {
-	    throw new RuntimeException(e);
-	}		
+	if (json.isNull(field)) {
+		return null;
+	} else {
+		try {
+			return json.getJSONArray(field);
+		} catch (JSONException e) {
+		    throw new RuntimeException(e);
+		}		
+	}
   }
   
   static JSONObject getObjectAt(JSONArray json, int index) {
@@ -30,7 +38,7 @@ public class Json {
 		return json.getJSONObject(index);
 	} catch (JSONException e) {
 	    throw new RuntimeException(e);
-	}		
+	}	
   }
   
   static String strAt(JSONArray json, int index) {
@@ -38,22 +46,30 @@ public class Json {
 		return json.getString(index);
 	} catch (JSONException e) {
 	    throw new RuntimeException(e);
-	}		
+	}	
   }
 
   static Date date(JSONObject json, String field) {
-	try {
-		return Atlas.ATLAS_DATE_FORMAT.parse(str(json, field));
-	} catch (ParseException e) {
-	      throw new RuntimeException(e);
-	}		
+	if (json.isNull(field)) {
+		return null;
+	} else {
+		try {
+			return Atlas.ATLAS_DATE_FORMAT.parse(str(json, field));
+		} catch (ParseException e) {
+		      throw new RuntimeException(e);
+		}	
+	}
   }
   
   static String str(JSONObject json, String field) {
-    try {
-      return json.getString(field);
-    } catch (JSONException e) {
-      throw new RuntimeException(e);
-    }
+	if (json.isNull(field)) {
+		return null;
+	} else {
+	    try {
+	      return json.getString(field);
+	    } catch (JSONException e) {
+	      throw new RuntimeException(e);
+	    }	
+	}
   }
 }
