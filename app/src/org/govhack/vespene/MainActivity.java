83501559d8fe61c97bd2669a3563e835be7eb058
Daniel Danilatos <daniel@helixta.com.au>
2013-06-02 09:58:03 +1000

    warm welcome splash screen

--- app/src/org/govhack/vespene/MainActivity.java
+++ app/src/org/govhack/vespene/MainActivity.java
@@ -14,12 +14,15 @@ import org.json.JSONObject;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.FragmentManager;
 import android.app.FragmentManager.OnBackStackChangedListener;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.AbstractCursor;
 import android.database.Cursor;
+import android.graphics.Typeface;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -34,8 +37,10 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import com.google.android.gms.location.LocationListener;
 
@@ -87,7 +92,8 @@ public class MainActivity extends Activity implements OnInitListener, LocationLi
         }
       }
     });
-
+    
+    showWarmWelcome();
 //    mp = MixpanelAPI.getInstance(this, MP_API_TOKEN);
 //    mp.identify(Installation.id(this));
 //    if (Installation.wasNewInstallation()) {
@@ -98,7 +104,45 @@ public class MainActivity extends Activity implements OnInitListener, LocationLi
 //    tts = new TextToSpeech(this, this);
   }
 
-@Override
+  private void showWarmWelcome() {
+	  View view = getLayoutInflater().inflate(R.layout.warm_welcome, null);
+	  if (view == null) {
+		  return;
+	  }
+	  
+	  ImageView splashImage = (ImageView) view.findViewById(R.id.splash_image);
+	  splashImage.setScaleType(ImageView.ScaleType.CENTER_CROP);
+	  
+	  Typeface tfReg = Typeface.createFromAsset(this.getAssets(), "fonts/Roboto-Regular.ttf");
+	  Typeface tfThin = Typeface.createFromAsset(this.getAssets(), "fonts/Roboto-Light.ttf");
+	  
+	  TextView topText = (TextView) view.findViewById(R.id.warm_text1);
+	  topText.setTypeface(tfThin);
+	  topText.setTextSize(12.0f);
+	  topText.setText(
+	      "Oz Explore helps you uncover the hidden gems of Australia. " +
+        "As you explore on foot, bicycle or by car, information about the " +
+	      "attractions near you will rise to the top.");
+	  
+	  TextView bottomText = (TextView) view.findViewById(R.id.warm_text2);
+	  bottomText.setTypeface(tfReg);
+	  bottomText.setTextSize(14.0f);
+	  bottomText.setText(
+        "- Tap a card to see more information.\n\n" +
+	      "- Search for attractions near other locations.\n");
+
+	  new AlertDialog.Builder(this)
+	    .setTitle("Welcome to Oz Explore")
+	    .setView(view)
+	    .setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+	        public void onClick(DialogInterface dialog, int which) { 
+	            // continue with delete
+	        }
+	     })
+	     .show();
+  }
+
+  @Override
   protected void onNewIntent(Intent i) {
     Log.i(TAG, "New intent: " + i.getAction());
     if (ACTION_LATEST.equals(i.getAction())) {
