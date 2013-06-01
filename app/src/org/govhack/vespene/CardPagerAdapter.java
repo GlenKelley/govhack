8571044f749490c940cb1c2672b2ff7649160ee0
Oleg Sushkov <osushkov@gmail.com>
2013-06-01 12:35:35 +1000

    basic cards stuff with images

--- app/src/org/govhack/vespene/CardPagerAdapter.java
+++ app/src/org/govhack/vespene/CardPagerAdapter.java
@@ -2,7 +2,10 @@ package org.govhack.vespene;
 
 import org.govhack.vespene.R;
 
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
@@ -10,6 +13,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CardPagerAdapter extends BaseAdapter {
   private static final String TAG = "CardPagerAdapter";
@@ -42,15 +46,40 @@ public class CardPagerAdapter extends BaseAdapter {
   public View getView(int position, View convertView, ViewGroup parent) {
     ViewGroup cardView;
     if (convertView == null) {  // if it's not recycled, initialize some attributes
-      cardView = (ViewGroup) activity.getLayoutInflater().inflate(R.layout.card, parent, false);
-      LinearLayout topBarHolder = (LinearLayout) cardView.findViewById(R.id.top_bar_holder);
-      TextView text = new TextView(activity);
-      text.setText("bla bla");
-      topBarHolder.addView(text);
+      cardView = inflateCard(parent);
     } else {
       cardView = (ViewGroup) convertView;
     }
 
+    return cardView;
+  }
+  
+  private ViewGroup inflateCard(ViewGroup parent) {
+    ViewGroup cardView = 
+        (ViewGroup) activity.getLayoutInflater().inflate(R.layout.card, parent, false);
+    
+    cardView.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Toast.makeText(activity, "Clicked on Attraction", Toast.LENGTH_SHORT).show();
+
+      }
+    });
+
+    LinearLayout topBarHolder = (LinearLayout) cardView.findViewById(R.id.top_bar_holder);
+    TextView text = new TextView(activity);
+    text.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD);
+    text.setTextSize(20.0f);
+    text.setText("Attraction Title");
+    topBarHolder.addView(text);
+    
+    LinearLayout thumbnailHolder = (LinearLayout) cardView.findViewById(R.id.thumbnail_holder);
+    ImageView thumbnail = new ImageView(activity);
+    thumbnail.setImageResource(SADS[0]);
+    thumbnailHolder.addView(thumbnail);
+    
+    //thumbnailHolder.requestLayout();
+    //cardView.requestLayout();
     return cardView;
   }
 }
