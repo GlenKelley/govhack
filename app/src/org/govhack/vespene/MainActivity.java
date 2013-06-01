70dd58a5dabd78da71169a3769e4f6ff5ba84739
Glen Kelley <glen.t.kelley@gmail.com>
2013-06-01 17:15:37 +1000

    added missing typo commit

--- app/src/org/govhack/vespene/MainActivity.java
+++ app/src/org/govhack/vespene/MainActivity.java
@@ -107,7 +107,7 @@ public class MainActivity extends Activity implements OnInitListener {
     Log.d(TAG, "onStart");
     super.onStart();
     track("app-start");
-	tts.setLanguage(Locale.US);
+//	tts.setLanguage(Locale.US);
   }
 
   @Override
