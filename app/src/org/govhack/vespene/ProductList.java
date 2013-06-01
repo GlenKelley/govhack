95eba923650fe61b45c3bc1fdb35b605d98ae95b
Daniel Danilatos <daniel@helixta.com.au>
2013-06-01 13:56:39 +1000

    improved invariants

--- app/src/org/govhack/vespene/ProductList.java
+++ app/src/org/govhack/vespene/ProductList.java
@@ -11,30 +11,50 @@ import org.govhack.vespene.util.Preconditions;
 
 public class ProductList {
   public interface Listener {
-    void onChanged();
+    /**
+     * May be called multiple times in a row before onUpdate
+     */
+    void onSearching();
+    /**
+     * Will not be called twice in a row, will always be preceded by onSearching
+     */
+    void onUpdate();
+    
     void onError(Exception e);
   }
 
   private final Atlas atlas;
   private Listener listener;
   private final List<Product> products = Lists.newArrayList();
+  private int currentSearchId = 0;
+  
   public ProductList(Atlas atlas) {
     this.atlas = atlas;
   }
   
   public void setListener(Listener l) {
-    this.listener = l;
+    this.listener = Preconditions.checkNotNull(l, "Null listener");
   }
   
   public void doSearch(Search search) {
     Preconditions.checkState(listener != null, "Listener not initialised");
+    final int searchId = ++currentSearchId;
+    listener.onSearching();
     atlas.search(search, new Callback<List<Product>>() {
       @Override
       public void success(List<Product> result) {
-        // TODO Auto-generated method stub
-        
+        if (searchId != currentSearchId) {
+          return;
+        }
+        products.clear();
+        products.addAll(result);
+        listener.onUpdate();
       }
       @Override public void error(Exception e) {
+        if (searchId != currentSearchId) {
+          return;
+        }
+        
         listener.onError(e);
       }
     });
